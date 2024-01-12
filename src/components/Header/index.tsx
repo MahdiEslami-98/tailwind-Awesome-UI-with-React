4c6182dd5157c0dc26e18587419e49274e8580ab
Mahdi Eslami <eslami.mahdi.98@gmail.com>
2024-01-12 14:12:47 +0330

    first-commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { LogoIcon } from "@/utils/icons";
+
+const Header = () => {
+  return (
+    <div className="px-6 ">
+      <div className="flex ">
+        <LogoIcon />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
